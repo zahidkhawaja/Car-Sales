e76ac1255a6e59396442f0c9e6e7e1aca9f48e3b
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-17 20:25:37 -0400

    Update price when adding features

--- src/components/Total.js
+++ src/components/Total.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from "react-redux";
 
 const Total = props => {
+  console.log(props);
   return (
     <div className="content">
       <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
--- src/reducers/featureReducer.js
+++ src/reducers/featureReducer.js
@@ -22,6 +22,7 @@ export const featureReducer = (state = initialState, action) => {
     case ADD_FEATURE:
       return {
         ...state,
+        additionalPrice: state.additionalPrice + action.payload.price,
         car: {
           ...state.car,
           features: [
