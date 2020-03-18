594c88f2c6bed1d42e3ca32d93ec57e610e0d5ae
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-17 22:06:31 -0400

    Fix remove feature functionality

--- src/reducers/featureReducer.js
+++ src/reducers/featureReducer.js
@@ -42,9 +42,8 @@ export const featureReducer = (state = initialState, action) => {
         additionalPrice: state.additionalPrice - action.payload.price,
         car: {
           ...state.car,
-          features: [
-            state.car.features.filter(feature => feature == action.payload)
-          ]
+          // Note: .filter() returns an array, so no need for brackets
+          features: state.car.features.filter(feature => feature.id !== action.payload.id)
         }
       }
     default:
